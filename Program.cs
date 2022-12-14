76a36d517b83779b757f973acd37152c9a3b03f2
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2022-12-14 22:53:48 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,24 +10,22 @@ namespace ConsoleApp1
 
     internal class Program
     {
-        private static int f(int n)
+        private static int f(int n) 
         {
-            if (n <= 0)
-                return -1;
-            else if (n == 1 || n == 2)
-                return 1;
+            if (n == 1 || n == 0) //假設 n=1,0回傳n值。
+                return n;
             else
-                return f(n - 1) + f(n - 2);
+                return f(n - 1) + f(n - 2);  //費式數列，數字為前兩個數相加。
         }
         static void Main(string[] args)   
         {
             Console.WriteLine("f(n):");
-            string max = Console.ReadLine();
+            string number = Console.ReadLine();
             int range = 0;
-            int.TryParse(max, out range); 
+            int.TryParse(number, out range); 
 
             for (int i = 1; i <= range; i++)
-                Console.WriteLine(f(i));
+                Console.WriteLine(f(i)+"(第"+i+"個數字)");
         }
 
     }
